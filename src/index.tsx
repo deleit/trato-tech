8fb2a061b4ccc9ed028dab058c285e9486a9afae
Deivison Leite <dlmacedo9@gmail.com>
2022-11-29 16:35:56 -0300

    Adição de Redux

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Router from './routes';
+import store from './store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router />
+    <Provider store={store}>
+      <Router />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
